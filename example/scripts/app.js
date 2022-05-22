20896306ad49f00a53b8338b3190178909fbde86
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-05-22 16:35:22 -0700

    Mas Suporte Para Node
    Improved support for Node! Now things run without error when loading `app.js`, awesome!

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -1,27 +1,23 @@
 // ES Module imports
+import { promises as fs } from "fs";
 import * as nbt from "../../index.js";
 
-window.nbt = nbt;
-
 // Fetch NBT data
-const BEDROCK_NBT = await (await fetch("./nbt/level.dat")).arrayBuffer();
-const JAVA_NBT = await (await fetch("./nbt/hello_world.nbt")).arrayBuffer();
-
-window.BEDROCK_NBT = BEDROCK_NBT;
-window.JAVA_NBT = JAVA_NBT;
+const BEDROCK_NBT = await fs.readFile("../nbt/level.dat");
+const JAVA_NBT = await fs.readFile("../nbt/hello_world.nbt");
 
 // Working with the NBT data
 console.log(BEDROCK_NBT);
 console.log(JAVA_NBT);
 
-const BEDROCK_OUTPUT = nbt.parse(BEDROCK_NBT,{ endian: "little" });
-const JAVA_OUTPUT = nbt.parse(JAVA_NBT);
+const BEDROCK_OUTPUT = await nbt.parse(BEDROCK_NBT,{ endian: "little" });
+const JAVA_OUTPUT = await nbt.parse(JAVA_NBT);
 
 console.log(BEDROCK_OUTPUT);
 console.log(JAVA_OUTPUT);
 
-const BEDROCK_RECOMPILE = nbt.write(BEDROCK_OUTPUT,{ endian: "little" });
-const JAVA_RECOMPILE = nbt.write(JAVA_OUTPUT,{ gzip: false });
+const BEDROCK_RECOMPILE = Buffer.from(await nbt.write(BEDROCK_OUTPUT,{ endian: "little" }));
+const JAVA_RECOMPILE = Buffer.from(await nbt.write(JAVA_OUTPUT,{ gzip: false }));
 
 console.log(BEDROCK_RECOMPILE);
 console.log(JAVA_RECOMPILE);
