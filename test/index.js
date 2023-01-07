05d78ff7aa42eb6042fdba788b29c5ab0aa9a944
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-07 12:31:20 -0800

    Class Compounds + Object Errors: Part One
    This is the class that I made to work on the Class-Generated Compound Tags + Object Error Handling feature.

--- test/index.js
+++ test/index.js
@@ -3,13 +3,27 @@
 import * as fs from "node:fs/promises";
 import * as NBT from "../dist/index.js";
 
-const data = await fs.readFile(new URL("./nbt/mca-chunk.nbt",import.meta.url));
-console.log(data.buffer,"\n");
+class NBTSourceClass {
+  InvalidRegExpObject = new RegExp(/searcher/);
+  NonCompatibleTextDecoder = new TextDecoder();
+  Func = () => {
+    return "This will not serialize to NBT";
+  }
+  WillPass = {
+    ByteTag: new NBT.Byte(127),
+    ShortTag: new NBT.Short(258),
+    DoubleTag: 84246,
+    LongTag: 30154000n,
+    CompoundTag: {
+      ThisIsAnotherCompoundTag: true
+    },
+    IntArrayTag: new Int32Array([45,8,6,3,2,345,67,887452,123123,254]),
+    LongArrayTag: new BigInt64Array([34234n,2343464756n,23425457n])
+  }
+}
 
-const result = await NBT.read(data.buffer);
-console.log(result,"\n");
+const source = new NBTSourceClass();
+console.log(source);
 
-const recompile = await NBT.write(result);
-console.log(recompile.buffer,"\n");
-
-console.log(Buffer.compare(data,recompile));
+const reversify = await NBT.write(source).then(NBT.read);
+console.log(reversify);
