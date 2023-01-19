034f7f5ba43ce3e910b25935093bbfe85b995d7b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-19 12:27:20 -0800

    Object Type Validation
    Going back to checking object literals once again, now that I essentially have the compound class handling "solved" in the meantime.
    
    Thought of one more way to declare a `satisfies` index signature; not sure if I like it, but I'm gonna document it as an option.
    
    ```ts
    // Another syntax option, feels a bit more verbose though, and it lines up with optional parameters (I don't like that)
    // Note that (currently at least) this does not make `string` optional, while it may seem like it could do that
    interface MyChecker {
      [property: string]?: string;
    }
    ```

--- test/index.js
+++ test/index.js
@@ -3,44 +3,41 @@
 import * as fs from "node:fs/promises";
 import * as NBT from "../dist/index.js";
 
-class NBTSourceClass {
-  InvalidRegExpObject = new RegExp(/searcher/);
-  NonCompatibleTextDecoder = new TextDecoder();
-  Func = () => {
+/** @type { NBT.CompoundTag } */
+const source = {
+  ByteTag: new NBT.Byte(127),
+  ShortTag: new NBT.Short(258),
+  DoubleTag: 84246,
+  LongTag: 30154000n,
+  CompoundTag: {
+    ThisIsAnotherCompoundTag: true
+  },
+  EmptyList: [],
+  InvalidRegExpObject: new RegExp(/searcher/),
+  NonCompatibleTextDecoder: new TextDecoder(),
+  Func: () => {
     return "This will not serialize to NBT"
-  };
+  },
   Method() {
     return "This won't be parseable either"
-  };
-  Symbol = Symbol(25);
-  Undefined = undefined;
-  Null = null;
-  WillPass = {
-    ByteTag: new NBT.Byte(127),
-    ShortTag: new NBT.Short(258),
-    DoubleTag: 84246,
-    LongTag: 30154000n,
-    CompoundTag: {
-      ThisIsAnotherCompoundTag: true
-    },
-    EmptyList: [],
-    InvalidListItems: [
-      [new RegExp(/searcher/)],
-      [new TextDecoder()],
-      [() => {
-        return "This will not serialize to NBT"
-      }],
-      [Symbol(25)],
-      [undefined],
-      [null]
-    ],
-    IntArrayTag: new Int32Array([45,8,6,3,2,345,67,887452,123123,254]),
-    LongArrayTag: new BigInt64Array([34234n,2343464756n,23425457n])
-  }
-}
+  },
+  Symbol: Symbol(25),
+  Undefined: undefined,
+  Null: null,
+  InvalidListItems: [
+    [new RegExp(/searcher/)],
+    [new TextDecoder()],
+    [() => {
+      return "This will not serialize to NBT"
+    }],
+    [Symbol(25)],
+    [undefined],
+    [null]
+  ],
+  IntArrayTag: new Int32Array([45,8,6,3,2,345,67,887452,123123,254]),
+  LongArrayTag: new BigInt64Array([34234n,2343464756n,23425457n])
+};
+console.log(source);
 
-const source = new NBTSourceClass();
-console.log(source.WillPass);
-
-const reversify = await NBT.write({ ...source }).then(NBT.read);
-console.log(reversify.data.WillPass);
+const reversify = await NBT.write(source).then(NBT.read);
+console.log(reversify.data);
