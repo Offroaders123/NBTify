264f28b3ceea18ba2f79cb56ae5904dfac3f5fe5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-27 03:59:06 -0800

    SNBT Indentation
    Wow! Been working on this for a few hours now. Had it almost working a few times along the way, so I would restart with what I had learned to make it right to get even closer to having everything indented correctly.
    
    This looks great! There's only a little more formatting I want to add to non-structure-based tags, so primitive-based arrays will still have a little bit of spacing, but not be multi-lined.
    
    This is a super huge benefit to having my own object parser! I would have loved to have these kinds of checks for my JSON formatting in STE back in the day. The ability to format arrays differently, depending on what is in each array, is super awesome. I want the TypedArray tags to be single-lined, since they tend to be really long, the primitive item arrays to be singled lined (string arrays may be one I might make multi-line though), and the arrays which *hold* either ListTag, CompoundTag, or the TypedArray tags will be multi-lined. So the only one left to implement from those, which isn't already added to match those are the possible string arrays, and pretty but single lined, primitive arrays. I think I will keep the TypedArray tags flattened to not have any whitespace. I can look into seeing if it looks nice though :)
    
    Listened to Blackwater Park for the second time tonight. I love it a ton! It was musically easy to comprehend this time around. Gonna listen to that some more this week, and also gonna go further into Mike Keneally's catalog. Listened to Incubus Science today, did not expect that! It sounded very Keneally-ish, completely different from their sound on later albums. It was sick! Glad I mentioned that right now to myself, now I'm gonna make it a plan to listen to it again tomorrow! And Blackwater Park :8

--- src/string.ts
+++ src/string.ts
@@ -10,7 +10,10 @@ const FLOAT_PATTERN = /^([-+]?(?:[0-9]+[.]?|[0-9]*[.][0-9]+)(?:e[-+]?[0-9]+)?)([
 const TRUE_PATTERN = /^true$/i;
 const FALSE_PATTERN = /^false$/i;
 
-export function stringify(value: Tag): string {
+export function stringify(value: Tag, space: string | number = "", level = 1): string {
+  space = (typeof space === "number") ? "".padStart(space," ") : space;
+  const fancy = (space !== "");
+
   const type = getTagType(value);
   switch (type){
     case TAG.BYTE: return (typeof value === "boolean") ? `${value}` : `${value as ByteTag}b`;
@@ -19,14 +22,14 @@ export function stringify(value: Tag): string {
     case TAG.LONG: return `${value as LongTag}l`;
     case TAG.FLOAT: return `${value as FloatTag}f`;
     case TAG.DOUBLE: return `${value as DoubleTag}d`;
-    case TAG.BYTE_ARRAY: return `[B;${stringifyList([...value as ByteArrayTag].map(entry => new Byte(entry)))}]`;
+    case TAG.BYTE_ARRAY: return `[B;${stringifyList([...value as ByteArrayTag].map(entry => new Byte(entry)),space,level)}]`;
     case TAG.STRING: return escapeWithQuotes(value as StringTag);
-    case TAG.LIST: return `[${stringifyList(value as ListTag)}]`;
+    case TAG.LIST: return `[${stringifyList(value as ListTag,space,level)}]`;
     case TAG.COMPOUND: {
-      return `{${[...Object.entries(value as CompoundTag)].map(([key,value]) => `${stringifyKey(key)}:${stringify(value)}`).join(",")}}`;
+      return `{${[...Object.entries(value as CompoundTag)].map(([key,value]) => `${fancy ? `\n${"".padStart((space as string).length * level,space as string)}` : ""}${stringifyKey(key)}:${fancy ? " " : ""}${stringify(value,space,level + 1)}`).join(",")}${fancy && Object.keys(value).length !== 0 ? `\n${"".padStart(space.length * (level - 1),space)}` : ""}}`;
     }
-    case TAG.INT_ARRAY: return `[I;${stringifyList([...value as IntArrayTag].map(entry => new Int(entry)))}]`;
-    case TAG.LONG_ARRAY: return `[L;${stringifyList([...value as LongArrayTag] as LongTag[])}]`;
+    case TAG.INT_ARRAY: return `[I;${stringifyList([...value as IntArrayTag].map(entry => new Int(entry)),space,level)}]`;
+    case TAG.LONG_ARRAY: return `[L;${stringifyList([...value as LongArrayTag] as LongTag[],space,level)}]`;
     default: throw new Error("Invalid tag");
   }
 }
@@ -36,8 +39,11 @@ export function parse(text: string): Tag {
   return parser.parseTag();
 }
 
-function stringifyList(list: Tag[]) {
-  return list.map((tag) => stringify(tag)).join(",");
+function stringifyList(list: Tag[], space: string, level: number) {
+  const [template] = list;
+  const type = getTagType(template) as TAG;
+  const fancy = (space !== "" && list.length !== 0 && new Set<TAG>([TAG.BYTE_ARRAY,TAG.LIST,TAG.COMPOUND,TAG.INT_ARRAY,TAG.LONG_ARRAY]).has(type));
+  return `${list.map((tag) => `${fancy ? `\n${"".padStart(space.length * level,space)}` : ""}${stringify(tag,space,level + 1)}`).join(",")}${fancy ? `\n${"".padStart(space.length * (level - 1),space)}` : ""}`;
 }
 
 function stringifyKey(key: string) {
@@ -137,6 +143,9 @@ class TagParser {
     const tag: CompoundTag = {};
     while (this.#canRead() && this.#peek() != "}") {
       this.#skipWhitespace();
+      if (this.#peek() === "}"){
+        break;
+      }
 
       const key = this.#readString();
       if (key == null) {
--- test/index.js
+++ test/index.js
@@ -3,26 +3,37 @@
 import * as fs from "node:fs/promises";
 import * as NBT from "../dist/index.js";
 
-// const data = await fs.readFile(new URL("./nbt/unnamed.nbt",import.meta.url));
-// console.log(data,"\n");
+// const data = await fs.readFile(new URL("./nbt/level.dat",import.meta.url));
+// // console.log(data,"\n");
 
 // const result = await NBT.read(data);
 const result = new NBT.NBTData({
   Noice: true,
   WorkOnBooleanValuesPls: true,
-  AnotherProperty: "Bananrama!"
+  ByteTagValue: new NBT.Byte(125),
+  AnotherProperty: "Bananrama!",
+  CheckForNestedJSON: JSON.stringify({
+    myJSONKey: "It's value!",
+    aBooleanProperty: true
+  }),
+  NestedObject: {
+    MyList: [45,753,123,757456],
+    OpethBlackwaterPark: "yes",
+    EmptyCompoundObject: {},
+    EmptyArrayList: []
+  }
 });
-console.log(result,"\n");
+// console.log(result.data,"\n");
 
 const data = await NBT.write(result).then(Buffer.from);
-console.log(data,"\n");
+// console.log(data,"\n");
 
-const stringed = NBT.stringify(result.data);
+const stringed = NBT.stringify(result.data,2);
 console.log(stringed,"\n");
 
 const parsed = /** @type { NBT.CompoundTag } */
   (NBT.parse(stringed));
-console.log(parsed,"\n");
+// console.log(parsed,"\n");
 
 // Using the base 'result' NBTData object as the WriteOptions
 const recompile = await NBT.write(parsed,result); 
