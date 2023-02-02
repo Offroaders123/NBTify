5a1e047bcf10809c2f12958af22dd1b8f77a6c3a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-02-02 09:09:37 -0800

    Writer Class
    Moved the new function stack into the writer class, just like how the other modules are structured. I like the layout of using a class for these kinds of tasks, because there's some pieces of state that need to be mutated with each function call, and it's not always the most convenient to pass that state around between each function individually, so keeping it somewhere local where the functions can instead mutate on, seems a little nicer to understand for me. If you rely solely on return types for the function state to be passed around, it seems a bit more challenging to track where the state of the stack goes. So while this setup is a bit beefier, I like the more-easily configurable aspect to it.
    
    So, in this state, it's simply just a wrapper class with methods that work together using their parameters and return types. After this stage, I'm going to move the state out of the function parameters, and into the class instance itself, that was each function can access the same state, but it doesn't need to be passed into it directly.

--- src/stringify.ts
+++ src/stringify.ts
@@ -8,84 +8,94 @@ export function stringify(data: CompoundTag | NBTData, space: string | number = 
   if (data instanceof NBTData){
     data = data.data as CompoundTag;
   }
-  space = (typeof space === "number") ? " ".repeat(space) : space;
-  return writeTag(data,space);
+  const writer = new SNBTWriter();
+  return writer.write(data,space);
 }
 
-function writeTag(value: Tag, space: string, level = 1): string {
-  const type = getTagType(value);
-  switch (type){
-    case TAG.BYTE: return (typeof value === "boolean") ? writeBoolean(value as boolean) : writeByte((value as ByteTag).valueOf());
-    case TAG.SHORT: return writeShort((value as ShortTag).valueOf());
-    case TAG.INT: return writeInt((value as IntTag).valueOf());
-    case TAG.LONG: return writeLong(value as LongTag);
-    case TAG.FLOAT: return writeFloat((value as FloatTag).valueOf());
-    case TAG.DOUBLE: return writeDouble(value as DoubleTag);
-    case TAG.BYTE_ARRAY: return writeByteArray(value as ByteArrayTag,space,level);
-    case TAG.STRING: return writeString(value as StringTag);
-    case TAG.LIST: return writeList(value as ListTag,space,level);
-    case TAG.COMPOUND: return writeCompound(value as CompoundTag,space,level);
-    case TAG.INT_ARRAY: return writeIntArray(value as IntArrayTag,space,level);
-    case TAG.LONG_ARRAY: return writeLongArray(value as LongArrayTag,space,level);
-    default: throw new Error("Invalid tag");
+export class SNBTWriter {
+  write(data: CompoundTag | NBTData, space: string | number = ""): string {
+    if (data instanceof NBTData){
+      data = data.data as CompoundTag;
+    }
+    space = (typeof space === "number") ? " ".repeat(space) : space;
+    return this.#writeTag(data,space);
   }
-}
 
-function writeBoolean(value: boolean){
-  return `${value}`;
-}
+  #writeTag(value: Tag, space: string, level = 1): string {
+    const type = getTagType(value);
+    switch (type){
+      case TAG.BYTE: return (typeof value === "boolean") ? this.#writeBoolean(value as boolean) : this.#writeByte((value as ByteTag).valueOf());
+      case TAG.SHORT: return this.#writeShort((value as ShortTag).valueOf());
+      case TAG.INT: return this.#writeInt((value as IntTag).valueOf());
+      case TAG.LONG: return this.#writeLong(value as LongTag);
+      case TAG.FLOAT: return this.#writeFloat((value as FloatTag).valueOf());
+      case TAG.DOUBLE: return this.#writeDouble(value as DoubleTag);
+      case TAG.BYTE_ARRAY: return this.#writeByteArray(value as ByteArrayTag,space,level);
+      case TAG.STRING: return this.#writeString(value as StringTag);
+      case TAG.LIST: return this.#writeList(value as ListTag,space,level);
+      case TAG.COMPOUND: return this.#writeCompound(value as CompoundTag,space,level);
+      case TAG.INT_ARRAY: return this.#writeIntArray(value as IntArrayTag,space,level);
+      case TAG.LONG_ARRAY: return this.#writeLongArray(value as LongArrayTag,space,level);
+      default: throw new Error("Invalid tag");
+    }
+  }
 
-function writeByte(value: number){
-  return `${value}b`;
-}
+  #writeBoolean(value: boolean){
+    return `${value}`;
+  }
 
-function writeShort(value: number){
-  return `${value}s`;
-}
+  #writeByte(value: number){
+    return `${value}b`;
+  }
 
-function writeInt(value: number){
-  return `${value}`;
-}
+  #writeShort(value: number){
+    return `${value}s`;
+  }
 
-function writeLong(value: bigint){
-  return `${value}l`;
-}
+  #writeInt(value: number){
+    return `${value}`;
+  }
 
-function writeFloat(value: number){
-  return `${value}f`;
-}
+  #writeLong(value: bigint){
+    return `${value}l`;
+  }
 
-function writeDouble(value: number){
-  return `${value}d`;
-}
+  #writeFloat(value: number){
+    return `${value}f`;
+  }
 
-function writeByteArray(value: Int8Array, space: string, level: number){
-  return `[B;${[...value as ByteArrayTag].map(entry => new Byte(entry)).map(entry => writeTag(entry,space,level)).join() as string}]`;
-}
+  #writeDouble(value: number){
+    return `${value}d`;
+  }
 
-function writeString(value: string){
-  return escapeWithQuotes(value);
-}
+  #writeByteArray(value: Int8Array, space: string, level: number){
+    return `[B;${[...value as ByteArrayTag].map(entry => new Byte(entry)).map(entry => this.#writeTag(entry,space,level)).join() as string}]`;
+  }
 
-function writeList(value: ListTag, space: string, level: number){
-  const fancy = (space !== "");
-  value = (value as ListTag).filter((entry): entry is Tag => getTagType(entry) !== -1);
-  const type = (value.length !== 0) ? getTagType(value[0]) as TAG : TAG.END;
-  const isIndentedList = fancy && new Set<TAG>([TAG.BYTE_ARRAY,TAG.LIST,TAG.COMPOUND,TAG.INT_ARRAY,TAG.LONG_ARRAY]).has(type);
-  return `[${value.map(entry => `${isIndentedList ? `\n${space.repeat(level)}` : ""}${writeTag(entry,space,level + 1)}`).join(`,${fancy && !isIndentedList ? " " : ""}`)}${isIndentedList ? `\n${space.repeat(level - 1)}` : ""}]`;
-}
+  #writeString(value: string){
+    return escapeWithQuotes(value);
+  }
 
-function writeCompound(value: CompoundTag, space: string, level: number){
-  const fancy = (space !== "");
-  return `{${[...Object.entries(value as CompoundTag)].map(([key,value]) => `${fancy ? `\n${(space as string).repeat(level)}` : ""}${UNQUOTED_STRING_PATTERN.test(key) ? key : escapeWithQuotes(key)}:${fancy ? " " : ""}${writeTag(value,space,level + 1)}`).join(",")}${fancy && Object.keys(value).length !== 0 ? `\n${space.repeat(level - 1)}` : ""}}`;
-}
+  #writeList(value: ListTag, space: string, level: number){
+    const fancy = (space !== "");
+    value = (value as ListTag).filter((entry): entry is Tag => getTagType(entry) !== -1);
+    const type = (value.length !== 0) ? getTagType(value[0]) as TAG : TAG.END;
+    const isIndentedList = fancy && new Set<TAG>([TAG.BYTE_ARRAY,TAG.LIST,TAG.COMPOUND,TAG.INT_ARRAY,TAG.LONG_ARRAY]).has(type);
+    return `[${value.map(entry => `${isIndentedList ? `\n${space.repeat(level)}` : ""}${this.#writeTag(entry,space,level + 1)}`).join(`,${fancy && !isIndentedList ? " " : ""}`)}${isIndentedList ? `\n${space.repeat(level - 1)}` : ""}]`;
+  }
 
-function writeIntArray(value: Int32Array, space: string, level: number){
-  return `[I;${[...value as IntArrayTag].map(entry => new Int(entry)).map(entry => writeTag(entry,space,level)).join() as string}]`;
-}
+  #writeCompound(value: CompoundTag, space: string, level: number){
+    const fancy = (space !== "");
+    return `{${[...Object.entries(value as CompoundTag)].map(([key,value]) => `${fancy ? `\n${(space as string).repeat(level)}` : ""}${UNQUOTED_STRING_PATTERN.test(key) ? key : escapeWithQuotes(key)}:${fancy ? " " : ""}${this.#writeTag(value,space,level + 1)}`).join(",")}${fancy && Object.keys(value).length !== 0 ? `\n${space.repeat(level - 1)}` : ""}}`;
+  }
 
-function writeLongArray(value: BigInt64Array, space: string, level: number){
-  return `[L;${[...value as LongArrayTag].map(entry => writeTag(entry,space,level)).join() as string}]`;
+  #writeIntArray(value: Int32Array, space: string, level: number){
+    return `[I;${[...value as IntArrayTag].map(entry => new Int(entry)).map(entry => this.#writeTag(entry,space,level)).join() as string}]`;
+  }
+
+  #writeLongArray(value: BigInt64Array, space: string, level: number){
+    return `[L;${[...value as LongArrayTag].map(entry => this.#writeTag(entry,space,level)).join() as string}]`;
+  }
 }
 
 const SINGLE_QUOTE_ESCAPE_PATTERN = /['\\]/g;
