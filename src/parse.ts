7e681bafdd30eacd454a79c2ede030d094f4c9ed
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-05-30 01:09:37 -0700

    SNBT Test Refactor
    Moved the stringify logic into it's actual module, out of the parse module. Added general code formatting to make all of the new code more structurally similar to how I like things. Moved the parse logic over to a class-based implementation, with the same internal functions now being methods on the builder class, instead of private functions within the scope of the main `parse` function.
    
    Need to re-add the unsafe List and Compound tag handling to the stringify logic, and I discovered that NBTify's primitive number types aren't being used, and instead are being asymmetrically returned (in terms of the API representations at the type/primitive level) as a simple `string` type, which loses the specificity of the number primitive that it's representing.

--- src/parse.ts
+++ src/parse.ts
@@ -3,231 +3,251 @@ import { TAG, getTagType } from "./tag.js";
 
 import type { Tag, RootTag, ByteTag, BooleanTag, ShortTag, IntTag, LongTag, FloatTag, DoubleTag, ByteArrayTag, StringTag, ListTag, CompoundTag, IntArrayTag, LongArrayTag } from "./tag.js";
 
-const unquotedRegExp = /^[0-9A-Za-z.+_-]+$/
+const unquotedRegExp = /^[0-9A-Za-z.+_-]+$/;
 
-export interface StringifyOptions {
-  pretty?: boolean
-  breakLength?: number
-  quote?: "single" | "double"
+export function parse<T extends RootTag = any>(data: string): T {
+  return new SNBTReader().read<T>(data);
 }
 
-export function stringify(tag: RootTag, options: StringifyOptions = {}): string {
-  const pretty = !!options.pretty, breakLength = options.breakLength || 70
-  const quoteChar = options.quote == "single" ? "'" : options.quote == "double" ? '"' : null
-  const spaces = " ".repeat(4)
+export class SNBTReader {
+  #data!: string;
+  #index!: number;
+  #i!: number;
+  #char!: string;
 
-  function escapeString(text: string) {
-    let q = quoteChar ?? '"'
-    if (quoteChar == null) {
-      for (let i = 0; i < text.length && i < 8; i++) {
-        switch (text[i]) {
-          case "'": q = '"'; break
-          case '"': q = "'"; break
-          default: continue
+  read<T extends RootTag = any>(data: string): T {
+    this.#data = data;
+    this.#index = 0;
+    this.#i = 0;
+    this.#char = "";
+
+    return this.#readTag() as T;
+  }
+
+  #unexpectedEnd(): Error {
+    return new Error("Unexpected end");
+  }
+
+  #unexpectedChar(i?: number): Error {
+    if (i == null) i = this.#index;
+    return new Error(`Unexpected character ${this.#data[this.#index]} at position ${this.#index}`);
+  }
+
+  #skipWhitespace(): void {
+    while (this.#index < this.#data.length){
+      if (this.#data[this.#index] != " " && this.#data[this.#index] != "\n") return;
+      this.#index += 1;
+    }
+  }
+
+  #readTag(): Tag {
+    this.#skipWhitespace();
+
+    this.#i = this.#index;
+    this.#char = this.#data[this.#index];
+
+    switch (this.#char){
+      case "{": return (this.#index++,this.#readCompound());
+      case "[": return (this.#index++,this.#readList());
+      case '"':
+      case "'": return this.#readQuotedString();
+      default: {
+        const value = this.#readNumber();
+        if (value != null && (this.#index == this.#data.length || !unquotedRegExp.test(this.#data[this.#index]))){
+          return value;
         }
-        break
+        return this.#data.slice(this.#i,this.#index) + this.#readUnquotedString();
       }
     }
-    return `${q}${text.replace(RegExp(`[${q}\\\\]`, "g"), x => `\\${x}`)}${q}`
   }
 
-  function stringify(tag: Tag, depth: number): string {
-    const space = pretty ? " " : "", sep = pretty ? ", " : ","
-    if (tag instanceof Int8) return `${tag}b`
-    else if (tag instanceof Int16) return `${tag}s`
-    else if (tag instanceof Int32) return `${tag}`
-    else if (typeof tag == "bigint") return `${tag}l`
-    else if (tag instanceof Float32) return `${tag}f`
-    else if (typeof tag == "number")
-      return Number.isInteger(tag) ? `${tag}.0` : tag.toString()
-    else if (typeof tag == "string") return escapeString(tag)
-    else if (tag instanceof Int8Array) return `[B;${space}${[...tag].join(sep)}]`
-    else if (tag instanceof Int32Array) return `[I;${space}${[...tag].join(sep)}]`
-    else if (tag instanceof BigInt64Array) return `[L;${space}${[...tag].join(sep)}]`
-    else if (tag instanceof Array) {
-      const list = tag.map(tag => stringify(tag, depth + 1))
-      if (list.reduce((acc, x) => acc + x.length, 0) > breakLength
-        || list.some(text => text.includes("\n"))) {
-        return `[\n${list.map(text => spaces.repeat(depth)
-          + text).join(",\n")}\n${spaces.repeat(depth - 1)}]`
+  #readNumber(): ByteTag | ShortTag | IntTag | LongTag | FloatTag | DoubleTag | null {
+    if (!"-0123456789".includes(this.#data[this.#index])) return null;
+
+    this.#i = this.#index++;
+    let hasFloatingPoint = false;
+
+    while (this.#index < this.#data.length){
+      this.#char = this.#data[this.#index++];
+
+      if ("0123456789".includes(this.#char)) {
+        continue;
       } else {
-        return `[${list.join(sep)}]`
+        switch (this.#char.toLowerCase()){
+          case ".": {
+            if (hasFloatingPoint) return (this.#index--,null);
+            hasFloatingPoint = true;
+          }
+          case "f": return new Float32(+this.#data.slice(this.#i,this.#index - 1));
+          case "d": return +this.#data.slice(this.#i,this.#index - 1);
+          case "b": return new Int8(+this.#data.slice(this.#i,this.#index - 1));
+          case "s": return new Int16(+this.#data.slice(this.#i,this.#index - 1));
+          case "l": return BigInt(this.#data.slice(this.#i,this.#index - 1));
+          default: {
+            if (hasFloatingPoint) {
+              return +this.#data.slice(this.#i,--this.#index);
+            } else {
+              return new Int32(+this.#data.slice(this.#i,--this.#index));
+            }
+          }
+        }
       }
+    }
+
+    if (hasFloatingPoint){
+      return +this.#data.slice(this.#i,this.#index);
     } else {
-      const pairs = (Object.entries(tag)
-        .filter(([_, v]) => v != null))
-        .map(([key, tag]) => {
-          if (!unquotedRegExp.test(key)) key = escapeString(key)
-          return `${key}:${space}${stringify(tag!, depth + 1)}`
-        })
-      if (pretty && pairs.reduce((acc, x) => acc + x.length, 0) > breakLength) {
-        return `{\n${pairs.map(text => spaces.repeat(depth)
-          + text).join(",\n")}\n${spaces.repeat(depth - 1)}}`
+      return new Int32(+this.#data.slice(this.#i,this.#index));
+    }
+  }
+
+  #readString(): string {
+    if (this.#data[this.#index] == '"' || this.#data[this.#index] == "'"){
+      return this.#readQuotedString();
+    } else {
+      return this.#readUnquotedString();
+    }
+  }
+
+  #readUnquotedString(): string {
+    this.#i = this.#index;
+
+    while (this.#index < this.#data.length){
+      if (!unquotedRegExp.test(this.#data[this.#index])) break;
+      this.#index++;
+    }
+
+    if (this.#index - this.#i == 0){
+      throw (this.#index == this.#data.length) ? this.#unexpectedEnd() : this.#unexpectedChar();
+    }
+
+    return this.#data.slice(this.#i, this.#index);
+  }
+
+  #readQuotedString(): string {
+    const quoteChar = this.#data[this.#index];
+    this.#i = ++this.#index;
+    let string = "";
+
+    while (this.#index < this.#data.length){
+      this.#char = this.#data[this.#index++];
+
+      if (this.#char == "\\"){
+        string += this.#data.slice(this.#i,this.#index - 1) + this.#data[this.#index];
+        this.#i = ++this.#index;
+      } else if (this.#char == quoteChar){
+        return string + this.#data.slice(this.#i,this.#index - 1);
+      }
+    }
+
+    throw this.#unexpectedEnd();
+  }
+
+  #skipCommas(isFirst: boolean, end: string): void {
+    this.#skipWhitespace();
+
+    if (this.#data[this.#index] == ",") {
+      if (isFirst){
+        throw this.#unexpectedChar();
       } else {
-        return `{${space}${pairs.join(sep)}${space}}`
+        this.#index++;
+        this.#skipWhitespace();
       }
-    }
-  }
-  return stringify(tag as Tag, 1)
-}
-
-export function parse<T extends RootTag = any>(text: string): T {
-  let index = 0, i = 0, char = ""
-
-  const unexpectedEnd = () => new Error("Unexpected end")
-  const unexpectedChar = (i?: number): Error => {
-    if (i == null) i = index
-    return new Error(`Unexpected character ${text[index]} at position ${index}`)
-  }
-
-  function skipWhitespace(): void {
-    while (index < text.length) {
-      if (text[index] != " " && text[index] != "\n") return
-      index += 1
+    } else if (!isFirst && this.#data[this.#index] != end){
+      throw this.#unexpectedChar();
     }
   }
 
-  function readNumber(): ByteTag | ShortTag | IntTag | LongTag | FloatTag | DoubleTag | null {
-    if (!"-0123456789".includes(text[index])) return null
-    i = index++
-    let hasFloatingPoint = false
-    while (index < text.length) {
-      char = text[index++]
-      if ("0123456789".includes(char)) {
-        continue
-      } else if (char == ".") {
-        if (hasFloatingPoint) return (index-- , null)
-        hasFloatingPoint = true
-      } else if (char == "f" || char == "F") {
-        return new Float32(+text.slice(i, index - 1))
-      } else if (char == "d" || char == "D") {
-        return +text.slice(i, index - 1)
-      } else if (char == "b" || char == "B") {
-        return new Int8(+text.slice(i, index - 1))
-      } else if (char == "s" || char == "S") {
-        return new Int16(+text.slice(i, index - 1))
-      } else if (char == "l" || char == "L") {
-        return BigInt(text.slice(i, index - 1))
-      } else if (hasFloatingPoint) {
-        return +text.slice(i, --index)
-      } else return new Int32(+text.slice(i, --index))
-    }
-    if (hasFloatingPoint) return +text.slice(i, index)
-    else return new Int32(+text.slice(i, index))
-  }
+  #readArray(type: string): ByteArrayTag | ListTag | IntArrayTag | LongArrayTag {
+    const array: string[] = [];
 
-  function readUnquotedString(): string {
-    i = index
-    while (index < text.length) {
-      if (!unquotedRegExp.test(text[index])) break
-      index++
-    }
-    if (index - i == 0) throw index == text.length ? unexpectedEnd() : unexpectedChar()
-    return text.slice(i, index)
-  }
+    while (this.#index < this.#data.length) {
+      this.#skipCommas(array.length == 0, "]");
 
-  function readQuotedString(): string {
-    const quoteChar = text[index]
-    i = ++index
-    let string = ""
-    while (index < text.length) {
-      char = text[index++]
-      if (char == "\\") {
-        string += text.slice(i, index - 1) + text[index]
-        i = ++index
-      } else if (char == quoteChar) return string + text.slice(i, index - 1)
-    }
-    throw unexpectedEnd()
-  }
-
-  function readString(): string {
-    if (text[index] == '"' || text[index] == "'") return readQuotedString()
-    else return readUnquotedString()
-  }
-
-  function skipCommas(isFirst: boolean, end: string): void {
-    skipWhitespace()
-    if (text[index] == ",") {
-      if (isFirst) throw unexpectedChar()
-      else index++ , skipWhitespace()
-    } else if (!isFirst && text[index] != end) {
-      throw unexpectedChar()
-    }
-  }
-
-  function readCompound(): CompoundTag {
-    const entries: [string, Tag][] = []
-    let first = true
-    while (true) {
-      skipCommas(first, "}"), first = false
-      if (text[index] == "}") {
-        index++
-        return entries.reduce<CompoundTag>((obj, [k, v]) => (obj[k] = v, obj), {})
+      if (this.#data[this.#index] == "]") {
+        this.#index++;
+        switch (type){
+          case "B": return Int8Array.from(array.map(v => +v));
+          case "I": return Int32Array.from(array.map(v => +v));
+          case "L": return BigInt64Array.from(array.map(v => BigInt(v)));
+        }
       }
-      const key = readString()
-      skipWhitespace()
-      if (text[index++] != ":") throw unexpectedChar()
-      entries.push([key, parse()])
-    }
-  }
 
-  function readArray(type: string): ByteArrayTag | ListTag | IntArrayTag | LongArrayTag {
-    const array: string[] = []
-    while (index < text.length) {
-      skipCommas(array.length == 0, "]")
-      if (text[index] == "]") {
-        index++
-        if (type == "B") return Int8Array.from(array.map(v => +v))
-        else if (type == "I") return Int32Array.from(array.map(v => +v))
-        else if (type == "L") return BigInt64Array.from(array.map(v => BigInt(v)))
+      this.#i = this.#index;
+      if (this.#data[this.#index] == "-"){
+        this.#index++;
       }
-      i = index
-      if (text[index] == "-") index++
-      while (index < text.length) {
-        if (!"0123456789".includes(text[index])) break
-        index++
+
+      while (this.#index < this.#data.length) {
+        if (!"0123456789".includes(this.#data[this.#index])) break;
+        this.#index++;
       }
-      if (index - i == 0) throw unexpectedChar()
-      if (unquotedRegExp.test(text[index])) throw unexpectedChar()
-      array.push(text.slice(i, index))
-    }
-    throw unexpectedEnd()
-  }
 
-  function readList(): ByteArrayTag | ListTag | IntArrayTag | LongArrayTag {
-    if ("BILbil".includes(text[index]) && text[index + 1] == ";") {
-      return readArray(text[(index += 2) - 2].toUpperCase())
-    }
-    const array: ListTag = []
-    while (index < text.length) {
-      skipWhitespace()
-      if (text[index] == ",") {
-        if (array.length == 0) throw unexpectedChar()
-        else index++ , skipWhitespace()
-      } else if (array.length > 0 && text[index] != "]") {
-        throw unexpectedChar(index - 1)
+      if (this.#index - this.#i == 0){
+        throw this.#unexpectedChar();
       }
-      if (text[index] == "]") return (index++ , array)
-      array.push(parse())
+      if (unquotedRegExp.test(this.#data[this.#index])){
+        throw this.#unexpectedChar();
+      }
+
+      array.push(this.#data.slice(this.#i,this.#index));
     }
-    throw unexpectedEnd()
+
+    throw this.#unexpectedEnd();
   }
 
-  function parse(): Tag {
-    skipWhitespace()
-
-    i = index, char = text[index]
-    if (char == "{") return (index++ , readCompound())
-    else if (char == "[") return (index++ , readList())
-    else if (char == '"' || char == "'") return readQuotedString()
-
-    const value = readNumber()
-    if (value != null && (index == text.length || !unquotedRegExp.test(text[index]))) {
-      return value
+  #readList(): ByteArrayTag | ListTag | IntArrayTag | LongArrayTag {
+    if ("BILbil".includes(this.#data[this.#index]) && this.#data[this.#index + 1] == ";"){
+      return this.#readArray(this.#data[(this.#index += 2) - 2].toUpperCase());
     }
-    return text.slice(i, index) + readUnquotedString()
+
+    const array: ListTag = [];
+
+    while (this.#index < this.#data.length){
+      this.#skipWhitespace();
+
+      if (this.#data[this.#index] == ","){
+        if (array.length == 0){
+          throw this.#unexpectedChar();
+        } else {
+          this.#index++;
+          this.#skipWhitespace();
+        }
+      } else if (array.length > 0 && this.#data[this.#index] != "]"){
+        throw this.#unexpectedChar(this.#index - 1);
+      }
+
+      if (this.#data[this.#index] == "]"){
+        return (this.#index++,array);
+      }
+
+      array.push(this.#readTag());
+    }
+
+    throw this.#unexpectedEnd();
   }
 
-  const value = parse() as T
-  return value
+  #readCompound(): CompoundTag {
+    const entries: [string, Tag][] = [];
+    let first = true;
+
+    while (true){
+      this.#skipCommas(first,"}");
+      first = false;
+
+      if (this.#data[this.#index] == "}"){
+        this.#index++;
+        return entries.reduce<CompoundTag>((obj,[k,v]) => (obj[k] = v,obj),{});
+      }
+
+      const key = this.#readString();
+      this.#skipWhitespace();
+
+      if (this.#data[this.#index++] != ":"){
+        throw this.#unexpectedChar();
+      }
+
+      entries.push([key,this.#readTag()]);
+    }
+  }
 }
