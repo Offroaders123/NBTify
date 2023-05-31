2aa2782309040be8861d0a2391f7d4947135463f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-05-30 17:11:44 -0700

    Test Tidying (Base64 Demo)
    Cleaning up the demo tests folder a bit.
    
    Discovered some neat things you can do with converting between TypedArrays and Base64!
    https://stackoverflow.com/a/36046727/19389068
    
    ```ts
    declare const NBT: typeof import("nbtify");
    
    // An NBT file's buffer data
    declare const temp1: Uint8Array;
    
    const temp2 = btoa(String.fromCharCode.apply(null,temp1));
    
    // atob(temp2).split("").map(char => char.charCodeAt(0));
    const temp3 = Uint8Array.from(atob(temp2).split(""),char => char.charCodeAt(0));
    
    const temp4 = await NBT.read(temp3);
    ```

--- test/app.js
+++ test/app.js
@@ -6,15 +6,12 @@ const PLAYER_FILE_0 = new URL("./nbt/P_280dfc7dac2f_00000001_knarF_520.dat",impo
 const PLAYER_FILE_1 = new URL("./nbt/N_280dfc7dac2f_100000001_.dat",import.meta.url);
 
 const buffer = await fetch(PLAYER_FILE_0)
-  .then(response => response.arrayBuffer());
+  .then(response => response.arrayBuffer())
+  .then(buffer => new Uint8Array(buffer));
 console.log(buffer);
 
 const result = await NBT.read(buffer,{ strict: false });
 console.log(result);
 
-const { buffer: recompile } = await NBT.write(result);
+const recompile = await NBT.write(result);
 console.log(recompile);
-
-const definition = NBT.definition(result,{ name: "LegacyConsolePlayer" });
-
-document.write(`<pre><code>${definition}</code></pre>`);
