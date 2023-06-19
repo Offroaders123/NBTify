bb1f6cb5d96bb31d3347250072764765c70875a8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-06-18 21:39:38 -0700

    Reverting Top-Level Await
    #30

--- src/compression.ts
+++ src/compression.ts
@@ -1,4 +1,33 @@
-const NODE_DEFLATE_RAW_POLYFILL: boolean = await (async () => {
+/**
+ * Compresses a Uint8Array using a specific compression format.
+*/
+export async function compress(data: Uint8Array, format: CompressionFormat): Promise<Uint8Array> {
+  if (format === "deflate-raw" && await NODE_DEFLATE_RAW_POLYFILL){
+    return deflateRawPolyfill(data);
+  }
+  const compressionStream = new CompressionStream(format);
+  return pipeThroughCompressionStream(data,compressionStream);
+}
+
+/**
+ * Decompresses a Uint8Array using a specific decompression format.
+*/
+export async function decompress(data: Uint8Array, format: CompressionFormat): Promise<Uint8Array> {
+  if (format === "deflate-raw" && await NODE_DEFLATE_RAW_POLYFILL){
+    return inflateRawPolyfill(data);
+  }
+  const decompressionStream = new DecompressionStream(format);
+  return pipeThroughCompressionStream(data,decompressionStream);
+}
+
+async function pipeThroughCompressionStream(data: Uint8Array, stream: CompressionStream | DecompressionStream): Promise<Uint8Array> {
+  const { body } = new Response(data);
+  const readable = body!.pipeThrough(stream);
+  const buffer = await new Response(readable).arrayBuffer();
+  return new Uint8Array(buffer);
+}
+
+const NODE_DEFLATE_RAW_POLYFILL: Promise<boolean> = (async () => {
   try {
     new CompressionStream("deflate-raw");
     new DecompressionStream("deflate-raw");
@@ -12,35 +41,6 @@ const NODE_DEFLATE_RAW_POLYFILL: boolean = await (async () => {
     }
   }
 })();
-
-/**
- * Compresses a Uint8Array using a specific compression format.
-*/
-export async function compress(data: Uint8Array, format: CompressionFormat): Promise<Uint8Array> {
-  if (format === "deflate-raw" && NODE_DEFLATE_RAW_POLYFILL){
-    return deflateRawPolyfill(data);
-  }
-  const compressionStream = new CompressionStream(format);
-  return pipeThroughCompressionStream(data,compressionStream);
-}
-
-/**
- * Decompresses a Uint8Array using a specific decompression format.
-*/
-export async function decompress(data: Uint8Array, format: CompressionFormat): Promise<Uint8Array> {
-  if (format === "deflate-raw" && NODE_DEFLATE_RAW_POLYFILL){
-    return inflateRawPolyfill(data);
-  }
-  const decompressionStream = new DecompressionStream(format);
-  return pipeThroughCompressionStream(data,decompressionStream);
-}
-
-async function pipeThroughCompressionStream(data: Uint8Array, stream: CompressionStream | DecompressionStream): Promise<Uint8Array> {
-  const { body } = new Response(data);
-  const readable = body!.pipeThrough(stream);
-  const buffer = await new Response(readable).arrayBuffer();
-  return new Uint8Array(buffer);
-}
 
 async function deflateRawPolyfill(data: Uint8Array): Promise<Uint8Array> {
   const { promisify } = await import("node:util");
