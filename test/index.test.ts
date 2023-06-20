3f1391ba640f3a0f0c506a5fb9528229841d0738
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-06-20 02:38:13 -0700

    Test Runner: Read, Stringify, Parse, and Write!
    Set up full unit tests! Haven't made my own like these yet before, so it's going to take a few edits to make it nicer looking hehe.
    
    To help make it work I had to install `tsx`, which I am using for things already. Unrelated, I also updated `@types/node`.
    
    #24
    https://nodejs.org/api/test.html
    https://nodejs.org/docs/v9.9.0/api/assert.html#assert_legacy_mode

---
+++ test/index.test.ts
@@ -0,0 +1,36 @@
+import { describe, it } from "node:test";
+import { strictEqual } from "node:assert";
+import { readFile } from "node:fs/promises";
+import * as NBT from "../src/index.js";
+
+const FILES = {
+  BIGTEST: "bigtest.nbt",
+  EMPTY_LIST: "empty_list.nbt",
+  EXTREME: "extreme.nbt",
+  HELLO_WORLD: "hello_world.nbt",
+  BEDROCK_LEVEL: "level.dat",
+  MCA_CHUNK: "mca-chunk.nbt",
+  LCE_PLAYER_0: "N_280dfc7dac2f_100000001_.dat",
+  LCE_PLAYER_1: "P_280dfc7dac2f_00000001_knarF_520.dat",
+  RIDICULOUS: "ridiculous.nbt",
+  SIMPLE_HOUSE: "simple_house.nbt",
+  UNNAMED: "unnamed.nbt"
+} as const;
+
+describe("Read, Stringify, Parse and Write",() => {
+  for (const [FILE,PATH] of Object.entries(FILES)){
+    it(PATH,async () => {
+      const buffer = await readFile(new URL(`./nbt/${PATH}`,import.meta.url));
+      const strict = !FILE.startsWith("LCE_PLAYER");
+      const result = await NBT.read(buffer,{ strict });
+      const stringified = NBT.stringify(result);
+      const parsed = NBT.parse(stringified);
+      const recompile = await NBT.write(parsed,result);
+      if (!strict) return;
+      const { compression } = result;
+      const header = (compression !== null && compression !== "deflate-raw") ? 10 : 0;
+      const compare = Buffer.compare(buffer.subarray(header),recompile.subarray(header));
+      strictEqual(compare,0,`'${PATH}' does not symmetrically recompile`);
+    });
+  }
+});
