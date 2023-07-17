104efb61249df82773d502e8557b9a7507f2a658
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-07-17 03:19:59 -0700

    Declarative Declarations
    Related to the last commit, I'm deciding to use interfaces and objects directly to represent the NBT, rather than using interfaces and classes. I don't want inheritance to get in the way of the structure representations. It also makes it much easier for working with existing NBT files/structures, because you don't have to build a class from the declarative object (the data), instead the object is what you're using to work with the data.
    
    So, I guess I'm taking the same route as how NBTify's primitives themselves work, I hopefully want to have the smallest level of abstractions on top of the data, only representing things using primitives, and working with those primitives using functional implementations.

--- test/builder-class.ts
+++ test/builder-class.ts
@@ -4,23 +4,25 @@ import type { Tag, IntTag, CompoundTag } from "../src/index.js";
 
 export type Difficulty = IntTag<0 | 1 | 2 | 3>;
 
-export interface BedrockLevelDatLike extends CompoundTag {
+export interface LevelDat extends CompoundTag {
   Difficulty: Difficulty;
 }
 
-export class BedrockLevelDat implements BedrockLevelDatLike {
-  [name: string]: Tag;
-
-  Difficulty: Difficulty = new Int32(2);
+export function createLevelDat(): LevelDat {
+  return {
+    Difficulty: new Int32(2)
+  };
 }
 
-const levelDat = new BedrockLevelDat();
+const levelDat = createLevelDat();
 levelDat.Difficulty;
 
 // @ts-expect-error - Property 'NON_THINGO' comes from an index signature. ts(4111)
 levelDat.NON_THINGO
 
 await write(levelDat);
+
+// Using classes to build NBT objects is supported too, but less declarative, so I don't recommend it as much.
 
 // @ts-expect-error - Index signature for type 'string' is missing in type '(Anonymous class)'. ts(1360)
 await write(new class {} satisfies CompoundTag);
