7a173c7dcabc0c6e7978650e3f722d126d88857b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-09-11 16:13:29 -0700

    Top-Level Error-Handling
    Some links I forgot to add over the last few commits:
    
    https://stackoverflow.com/questions/40561591/node-package-json-bin-value-command-not-working
    https://stackoverflow.com/questions/30652243/how-to-pipe-output-of-a-node-cli-program-to-shell
    https://stackoverflow.com/questions/9191235/whats-the-right-way-to-exit-node-js-script-with-a-log-message
    https://www.geeksforgeeks.org/node-js-exit-codes/
    https://stackoverflow.com/questions/40867345/catch-all-uncaughtexception-for-node-js-app (for this commit)
    https://askubuntu.com/questions/420981/how-do-i-save-terminal-output-to-a-file (I want to get these working with both NBT and SNBT files)
    https://stackoverflow.com/questions/70765090/buffered-writes-to-stdout-in-node-js
    
    #25

--- src/bin.ts
+++ src/bin.ts
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
-import { argv } from "node:process";
 import { extname } from "node:path";
 import { readFile } from "node:fs/promises";
 import { read, parse } from "./index.js";
 
-const args = argv.slice(2);
-console.log(args);
+process.on("uncaughtException",event => {
+  console.error(`${event}`);
+  process.exit(1);
+});
+
+const args = process.argv.slice(2);
+// console.log(args);
 
 const [file] = args;
 
 if (file === undefined){
-  console.error("Missing argument 'input'.");
-  process.exit(1);
+  throw new Error("Missing argument 'input'");
 }
 
-const buffer = await readFile(file).catch(error => {
-  console.error(`${error}`);
-  process.exit(1);
-});
+const buffer = await readFile(file);
 
 if (extname(file) === ".snbt"){
   const snbt = parse(buffer.toString());
