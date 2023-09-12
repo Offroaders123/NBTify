c81e50efe32eb29da0d19f09e76305fda87c1ed5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-09-12 03:13:06 -0700

    Args Module
    Brought over a bit of inspiration of this from my Art-Gen project!
    
    I think it works really well to put all of your arguments into a separate module, to where you can just import them as distinct values. It makes it really easy to see what the actual running of your script does itself, without having to worry about the argument handling itself. Super cool!
    
    I have to add a proper type conversion to wrap the Bedrock Level value in an `Int32`, and also find a way to represent the Name and Compression arg `null` values in some way. It can't particularly work with "null", because you could possibly intend on wanting the string literal "null" itself, so I can't use that because of the overlap.
    
    Noticing it would work really easily to wrap these argument locating calls into standalone general functions, but I wanted to commit these changes before trying to do that. :)

---
+++ src/bin/args.ts
@@ -0,0 +1,38 @@
+import type { FormatOptions } from "../index.js";
+
+const SNBT_PATTERN = /^--snbt$/;
+const PIPE_PATTERN = /^--pipe$/;
+const NAME_PATTERN = /^--name=$/;
+const ENDIAN_PATTERN = /^--endian=$/;
+const COMPRESSION_PATTERN = /^--compression=$/;
+const BEDROCK_LEVEL_PATTERN = /^--bedrock-level=$/;
+
+const args: string[] = process.argv.slice(2);
+
+process.on("uncaughtException",error => {
+  console.error(`${error}`);
+  process.exit(1);
+});
+
+export const file: string = args.shift() ?? (() => {
+  throw new TypeError("Missing argument 'input'");
+})();
+
+for (const arg of args){
+  switch (true){
+    case SNBT_PATTERN.test(arg):
+    case PIPE_PATTERN.test(arg):
+    case NAME_PATTERN.test(arg):
+    case ENDIAN_PATTERN.test(arg):
+    case COMPRESSION_PATTERN.test(arg):
+    case BEDROCK_LEVEL_PATTERN.test(arg): break;
+    default: throw new TypeError(`Unexpected argument '${arg}'`);
+  }
+}
+
+export const snbt: boolean = args.some(arg => SNBT_PATTERN.test(arg));
+export const pipe: boolean = args.some(arg => PIPE_PATTERN.test(arg));
+export const name: FormatOptions["name"] = args.find(arg => NAME_PATTERN.test(arg))?.replace(NAME_PATTERN,"");
+export const endian: FormatOptions["endian"] = args.find(arg => ENDIAN_PATTERN.test(arg))?.replace(ENDIAN_PATTERN,"") as FormatOptions["endian"];
+export const compression: FormatOptions["compression"] = args.find(arg => COMPRESSION_PATTERN.test(arg))?.replace(COMPRESSION_PATTERN,"") as FormatOptions["compression"];
+export const bedrockLevel: FormatOptions["bedrockLevel"] = args.find(arg => BEDROCK_LEVEL_PATTERN.test(arg))?.replace(BEDROCK_LEVEL_PATTERN,"") as FormatOptions["bedrockLevel"];
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -3,25 +3,9 @@
 import { extname } from "node:path";
 import { readFile } from "node:fs/promises";
 import { read, write, parse, stringify, NBTData } from "../index.js";
+import { file, snbt, pipe, name, endian, compression, bedrockLevel } from "./args.js";
 
-import type { RootTagLike, Name, Endian, Compression, BedrockLevel } from "../index.js";
-
-process.on("uncaughtException",error => {
-  console.error(`${error}`);
-  process.exit(1);
-});
-
-const args = process.argv.slice(2);
-// console.log(args);
-
-const [file] = args;
-
-const snbt = args.some(arg => arg === "--snbt");
-const pipe = args.some(arg => arg === "--pipe");
-const name = args.find(arg => arg.startsWith("--name="))?.slice(7) as Name | undefined;
-const endian = args.find(arg => arg.startsWith("--endian="))?.slice(9) as Endian | undefined;
-const compression = args.find(arg => arg.startsWith("--compression="))?.slice(14) as Compression | undefined;
-const bedrockLevel = args.find(arg => arg.startsWith("--bedrock-level="))?.slice(16) as BedrockLevel | undefined;
+import type { RootTagLike } from "../index.js";
 
 // console.log({ name, endian, compression, bedrockLevel });
 
