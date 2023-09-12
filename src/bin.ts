9001892d1da36b2f06b03b6a9a9c65a6c9ca38b0
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-09-11 19:42:40 -0700

    Pipe Flag
    Made the pipe functionality optional with a flag, since it makes the console look a bit weird since that's where it uses stdout I think.

--- src/bin.ts
+++ src/bin.ts
@@ -17,12 +17,13 @@ const args = process.argv.slice(2);
 const [file] = args;
 
 const snbt = args.some(arg => arg === "--snbt");
+const pipe = args.some(arg => arg === "--pipe");
 const name = args.find(arg => arg.startsWith("--name="))?.slice(7) as Name | undefined;
 const endian = args.find(arg => arg.startsWith("--endian="))?.slice(9) as Endian | undefined;
 const compression = args.find(arg => arg.startsWith("--compression="))?.slice(14) as Compression | undefined;
 const bedrockLevel = args.find(arg => arg.startsWith("--bedrock-level="))?.slice(16) as BedrockLevel | undefined;
 
-console.log({ name, endian, compression, bedrockLevel });
+// console.log({ name, endian, compression, bedrockLevel });
 
 if (file === undefined){
   throw new Error("Missing argument 'input'");
@@ -33,8 +34,11 @@ const input = await readFile(file);
 const data: RootTag | NBTData = extname(file) === ".snbt" ? parse(input.toString()) : await read(input);
 
 const nbt: NBTData = new NBTData(data,{ name, endian, compression, bedrockLevel });
-console.log(snbt ? stringify(nbt,{ space: 2 }) : nbt);
+if (!pipe){
+  console.log(snbt ? stringify(nbt,{ space: 2 }) : nbt);
+}
 
-const output = snbt ? stringify(nbt,{ space: 2 }) : await write(nbt);
-
-await new Promise(resolve => process.stdout.write(output,resolve));
+if (pipe){
+  const output = snbt ? stringify(nbt,{ space: 2 }) : await write(nbt);
+  await new Promise(resolve => process.stdout.write(output,resolve));
+}
