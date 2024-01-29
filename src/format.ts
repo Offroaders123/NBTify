1372ef4122558e6a99c455edc7f93e507b67fabf
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-01-29 01:51:37 -0800

    Hybrid Constructor Validation
    I have been wanting to have something like this setup for some time now, I think I will try this again a little later, moving this style to other parts of the codebase as well.
    
    Having a loop for the property definitions is so much more simple now as well! I wanted to do that a while back, but I don't think I fully understood a way to do that just yet.
    
    This brings the library's behavior back to before the demo validation things. The wider-supported IO parameters is still in the codebase as well, going to see whether that should still stick around as well.

--- src/format.ts
+++ src/format.ts
@@ -18,11 +18,11 @@ export interface NBTDataOptions extends Partial<Format> {}
  * An object which represents a set of NBT data.
 */
 export class NBTData<T extends RootTagLike = RootTag> implements Format {
-  data: T;
-  rootName: RootName;
-  endian: Endian;
-  compression: Compression;
-  bedrockLevel: BedrockLevel;
+  readonly data: T;
+  readonly rootName: RootName;
+  readonly endian: Endian;
+  readonly compression: Compression;
+  readonly bedrockLevel: BedrockLevel;
 
   constructor(data: T | NBTData<T>, options: NBTDataOptions = {}) {
     if (data instanceof NBTData){
@@ -55,20 +55,20 @@ export class NBTData<T extends RootTagLike = RootTag> implements Format {
     this.compression = compression;
     this.bedrockLevel = bedrockLevel;
 
-    return new Proxy(this,{
-      set<K extends keyof NBTData<T>>(target: NBTData<T>, property: keyof NBTData<T>, value: NBTData<T>[K]): boolean {
-        switch (property){
-          case "data": isRootTagLike(value); break;
-          case "rootName": isRootName(value); break;
-          case "endian": isEndian(value); break;
-          case "compression": isCompression(value); break;
-          case "bedrockLevel": isBedrockLevel(value); break;
-        }
-        // @ts-expect-error
-        target[property] = value;
-        return true;
+    for (const property of ["data","rootName","endian","compression","bedrockLevel"] as (keyof NBTData)[]){
+      let enumerable: boolean = true;
+
+      switch (property){
+        case "compression": enumerable = compression !== null;
+        case "bedrockLevel": enumerable = bedrockLevel !== null;
       }
-    });
+
+      Object.defineProperty(this,property,{
+        configurable: true,
+        enumerable,
+        writable: false
+      });
+    }
   }
 
   get [Symbol.toStringTag]() {
@@ -76,38 +76,37 @@ export class NBTData<T extends RootTagLike = RootTag> implements Format {
   }
 }
 
-
 export function isRootTagLike(data: unknown): asserts data is RootTagLike {
   if (typeof data !== "object" || data === null){
-    throw new TypeError("First parameter must be an object or array");
+    throw new TypeError("Root Tag must be an object or array");
   }
   data satisfies RootTagLike;
 }
 
 export function isRootName(rootName: unknown): asserts rootName is RootName {
   if (typeof rootName !== "string" && rootName !== null){
-    throw new TypeError("Root Name option must be a string or null");
+    throw new TypeError("Root Name must be a string or null");
   }
   rootName satisfies RootName;
 }
 
 export function isEndian(endian: unknown): asserts endian is Endian {
   if (endian !== "big" && endian !== "little"){
-    throw new TypeError("Endian option must be a valid endian type");
+    throw new TypeError("Endian must be 'big' or 'little'");
   }
   endian satisfies Endian;
 }
 
 export function isCompression(compression: unknown): asserts compression is Compression {
   if (compression !== "deflate" && compression !== "deflate-raw" && compression !== "gzip" && compression !== null){
-    throw new TypeError("Compression option must be a valid compression type");
+    throw new TypeError("Compression must be 'deflate', 'deflate-raw', 'gzip', or null");
   }
   compression satisfies Compression;
 }
 
 export function isBedrockLevel(bedrockLevel: unknown): asserts bedrockLevel is BedrockLevel {
   if (typeof bedrockLevel !== "number" && bedrockLevel !== null){
-    throw new TypeError("Bedrock Level option must be a number or null");
+    throw new TypeError("Bedrock Level must be a number or null");
   }
   bedrockLevel satisfies BedrockLevel;
 }
