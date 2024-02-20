2d7d709f0938322cdb974b6e8485f44ade49d362
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-19 23:40:41 -0800

    CLI Simplification
    The winds are changing, as it seems.
    
    Spirits Will Collide
    
    Learning the difference between healing through getting closer to yourself, versus getting further away from yourself.
    
    Happy 21st :)

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -3,7 +3,7 @@
 import { extname } from "node:path";
 import { readFileSync } from "node:fs";
 import { inspect, promisify } from "node:util";
-import { read, write, parse, stringify, NBTData, Int32 } from "../index.js";
+import { read, write, parse, stringify, NBTData } from "../index.js";
 import { file, nbt, snbt, format, space } from "./args.js";
 
 import type { RootTag } from "../index.js";
@@ -31,24 +31,14 @@ if (file === 0){
 
 const output: NBTData = new NBTData(input,format);
 
-if (format.bedrockLevel){
-  if (format.endian !== "little"){
-    throw new TypeError("Endian option must be 'little' when the Bedrock Level flag is enabled");
-  }
-if (!("StorageVersion" in output.data) || !(output.data["StorageVersion"] instanceof Int32)){
-    throw new TypeError("Expected a 'StorageVersion' Int tag in file, when Bedrock Level flag is enabled");
-  }
-}
-
 if (!nbt && !snbt){
-  const result: string | NBTData = snbt
-    ? stringify(output,{ space: 2 })
-    : output;
-  console.log(inspect(result,{ colors: true, depth: null }));
+  console.log(inspect(output,{ colors: true, depth: null }));
   process.exit(0);
 }
+
+const stdoutWriteAsync = promisify(process.stdout.write.bind(process.stdout));
 
 const result: string | Uint8Array = snbt
   ? `${stringify(output,{ space: space ?? 2 })}\n`
   : await write(output);
-await promisify(process.stdout.write.bind(process.stdout))(result);
+await stdoutWriteAsync(result);
