05b0532e19363c475c58373b2d8e98e34e327c1f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-04 12:48:48 -0800

    Little Endian Parameter Usage
    Added Little Endian flag support to the reader functions again. It was just a matter of properly passing down the thing as a parameter between all of the functions.
    
    Euphoria, reminds me of JP - Terminal Velocity actually! :D

--- src/index.ts
+++ src/index.ts
@@ -21,41 +21,41 @@ console.log(read(fileDemo));
 
 // read
 
-function read(data: Uint8Array) {
+function read(data: Uint8Array, littleEndian: boolean = false) {
   const reader = new DataReader(data);
-  return readRoot(reader);
+  return readRoot(reader, littleEndian);
 }
 
-function readRoot(reader: DataReader): [string, RootTag] {
+function readRoot(reader: DataReader, littleEndian: boolean): [string, RootTag] {
   const type = reader.readUint8();
   if (type !== TAG.LIST && type !== TAG.COMPOUND){
     throw new Error(`Expected an opening List or Compound tag at the start of the buffer, encountered tag type '${type}'`);
   }
 
-  const rootName = readString(reader);
-  const root: RootTag = readTag(reader, type) as RootTag; // maybe make this generic as well?
+  const rootName = readString(reader, littleEndian);
+  const root: RootTag = readTag(reader, type, littleEndian) as RootTag; // maybe make this generic as well?
 
   return [rootName, root];
 }
 
-function readTag(reader: DataReader, type: TAG): Tag {
+function readTag(reader: DataReader, type: TAG, littleEndian: boolean): Tag {
   switch (type){
     case TAG.END: {
       const remaining = reader.data.byteLength - reader.byteOffset;
       throw new Error(`Encountered unexpected End tag at byte offset ${reader.byteOffset}, ${remaining} unread bytes remaining`);
     }
     case TAG.BYTE: return readByte(reader);
-    case TAG.SHORT: return readShort(reader);
-    case TAG.INT: return readInt(reader);
-    case TAG.LONG: return readLong(reader);
-    case TAG.FLOAT: return readFloat(reader);
-    case TAG.DOUBLE: return readDouble(reader);
-    case TAG.BYTE_ARRAY: return readByteArray(reader);
-    case TAG.STRING: return readString(reader);
-    case TAG.LIST: return readList(reader);
-    case TAG.COMPOUND: return readCompound(reader);
-    case TAG.INT_ARRAY: return readIntArray(reader);
-    case TAG.LONG_ARRAY: return readLongArray(reader);
+    case TAG.SHORT: return readShort(reader, littleEndian);
+    case TAG.INT: return readInt(reader, littleEndian);
+    case TAG.LONG: return readLong(reader, littleEndian);
+    case TAG.FLOAT: return readFloat(reader, littleEndian);
+    case TAG.DOUBLE: return readDouble(reader, littleEndian);
+    case TAG.BYTE_ARRAY: return readByteArray(reader, littleEndian);
+    case TAG.STRING: return readString(reader, littleEndian);
+    case TAG.LIST: return readList(reader, littleEndian);
+    case TAG.COMPOUND: return readCompound(reader, littleEndian);
+    case TAG.INT_ARRAY: return readIntArray(reader, littleEndian);
+    case TAG.LONG_ARRAY: return readLongArray(reader, littleEndian);
     default: throw new Error(`Encountered unsupported tag type '${type}' at byte offset ${reader.byteOffset}`);
   }
 }
@@ -64,38 +64,38 @@ function readByte(reader: DataReader): ByteTag {
   return new Int8(reader.readInt8());
 }
 
-function readShort(reader: DataReader): ShortTag {
-  return new Int16(reader.readInt16(false));
+function readShort(reader: DataReader, littleEndian: boolean): ShortTag {
+  return new Int16(reader.readInt16(littleEndian));
 }
 
-function readInt(reader: DataReader): IntTag {
-  return new Int32(reader.readInt32(false));
+function readInt(reader: DataReader, littleEndian: boolean): IntTag {
+  return new Int32(reader.readInt32(littleEndian));
 }
 
-function readLong(reader: DataReader): LongTag {
-  return reader.readBigInt64(false);
+function readLong(reader: DataReader, littleEndian: boolean): LongTag {
+  return reader.readBigInt64(littleEndian);
 }
 
-function readFloat(reader: DataReader): FloatTag {
-  return new Float32(reader.readFloat32(false));
+function readFloat(reader: DataReader, littleEndian: boolean): FloatTag {
+  return new Float32(reader.readFloat32(littleEndian));
 }
 
-function readDouble(reader: DataReader): DoubleTag {
-  return reader.readFloat64(false);
+function readDouble(reader: DataReader, littleEndian: boolean): DoubleTag {
+  return reader.readFloat64(littleEndian);
 }
 
-function readByteArray(reader: DataReader): ByteArrayTag {
-  return reader.readInt8Array(reader.readInt32(false));
+function readByteArray(reader: DataReader, littleEndian: boolean): ByteArrayTag {
+  return reader.readInt8Array(reader.readInt32(littleEndian));
 }
 
-function readString(reader: DataReader): StringTag {
-  const length = reader.readUint16(false);
+function readString(reader: DataReader, littleEndian: boolean): StringTag {
+  const length = reader.readUint16(littleEndian);
   return reader.readString(length);
 }
 
-function readList(reader: DataReader): ListTag<Tag> {
+function readList(reader: DataReader, littleEndian: boolean): ListTag<Tag> {
   const type = reader.readUint8();
-  const length = reader.readInt32(false);
+  const length = reader.readInt32(littleEndian);
   const value: ListTag<Tag> = [];
   Object.defineProperty(value,TAG_TYPE,{
     configurable: true,
@@ -104,31 +104,31 @@ function readList(reader: DataReader): ListTag<Tag> {
     value: type
   });
   for (let i = 0; i < length; i++){
-    const entry = readTag(reader, type);
+    const entry = readTag(reader, type, littleEndian);
     value.push(entry);
   }
   return value;
 }
 
-function readCompound(reader: DataReader): CompoundTag {
+function readCompound(reader: DataReader, littleEndian: boolean): CompoundTag {
   const value: CompoundTag = {};
   while (true){
     const type = reader.readUint8();
     if (type === TAG.END) break;
-    const nameLength = reader.readUint16(false);
+    const nameLength = reader.readUint16(littleEndian);
     const name = reader.readString(nameLength);
-    const entry = readTag(reader, type);
+    const entry = readTag(reader, type, littleEndian);
     value[name] = entry;
   }
   return value;
 }
 
-function readIntArray(reader: DataReader): IntArrayTag {
-  return reader.readInt32Array(reader.readInt32(false));
+function readIntArray(reader: DataReader, littleEndian: boolean): IntArrayTag {
+  return reader.readInt32Array(reader.readInt32(littleEndian), littleEndian);
 }
 
-function readLongArray(reader: DataReader): LongArrayTag {
-  return reader.readBigInt64Array(reader.readInt32(false));
+function readLongArray(reader: DataReader, littleEndian: boolean): LongArrayTag {
+  return reader.readBigInt64Array(reader.readInt32(littleEndian), littleEndian);
 }
 
 type ReaderMethod = {
@@ -204,19 +204,19 @@ class DataReader {
     return this.decoder.decode(this.data.subarray(this.byteOffset, this.byteOffset += length));
   }
 
-  readInt32Array(length: number): Int32Array {
+  readInt32Array(length: number, littleEndian: boolean): Int32Array {
     const value = new Int32Array(length);
     for (const i in value){
-      const entry = this.readInt32(false);
+      const entry = this.readInt32(littleEndian);
       value[i] = entry;
     }
     return value;
   }
 
-  readBigInt64Array(length: number): BigInt64Array {
+  readBigInt64Array(length: number, littleEndian: boolean): BigInt64Array {
     const value = new BigInt64Array(length);
     for (const i in value){
-      const entry = this.readBigInt64(false);
+      const entry = this.readBigInt64(littleEndian);
       value[i] = entry;
     }
     return value;
