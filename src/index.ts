d0f76f7ff6a6baefb31ca61e121316c6ad306058
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-04 11:54:08 -0800

    Generic Tape Readers/Writers
    This is kind of inspired by the original shape that NBT.js uses, but without the NBT-specific features.
    
    I think I can write a functional part on top of this implementation maybe, since I think it might be the only state-related thing. But maybe not, we'll see. I'm not sure how appending to lists or compounds can work if one doesn't use a stateful class. I want simple function return types too, I don't want to have to nest things complexly, just for the sake of not using classes.
    
    Scambot 2! Brings me back, crazy I've already had it in my music collection for two years now :O

--- src/index.ts
+++ src/index.ts
@@ -21,23 +21,160 @@ console.log(read(fileDemo));
 
 // read
 
-function read(data: Uint8Array){
-  return new NBTReader().readRoot(data);
+function read(data: Uint8Array) {}
+
+type ReaderMethod = {
+  [K in keyof DataView]: K extends `get${infer T}` ? T : never;
+}[keyof DataView];
+
+class DataReader {
+  private byteOffset: number;
+  private data: Uint8Array;
+  private view: DataView;
+
+  constructor(data: Uint8Array) {
+    this.byteOffset = 0;
+    this.data = data;
+    this.view = new DataView(data.buffer, data.byteOffset, data.byteLength);
+  }
+
+  readUint8(): number {
+    return this.read("Uint8", 1, false);
+  }
+
+  readInt8(): number {
+    return this.read("Int8", 1, false);
+  }
+
+  readUint16(littleEndian: boolean): number {
+    return this.read("Uint16", 2, littleEndian);
+  }
+
+  readInt16(littleEndian: boolean): number {
+    return this.read("Int16", 2, littleEndian);
+  }
+
+  readUint32(littleEndian: boolean): number {
+    return this.read("Uint32", 4, littleEndian);
+  }
+
+  readInt32(littleEndian: boolean): number {
+    return this.read("Int32", 4, littleEndian);
+  }
+
+  readFloat32(littleEndian: boolean): number {
+    return this.read("Float32", 4, littleEndian);
+  }
+
+  readFloat64(littleEndian: boolean): number {
+    return this.read("Float64", 8, littleEndian);
+  }
+
+  readBigUint64(littleEndian: boolean): bigint {
+    return this.read("BigUint64", 8, littleEndian);
+  }
+
+  readBigInt64(littleEndian: boolean): bigint {
+    return this.read("BigInt64", 8, littleEndian);
+  }
+
+  private read<T extends ReaderMethod>(type: T, byteLength: number, littleEndian: boolean): ReturnType<DataView[`get${T}`]>;
+  private read(type: ReaderMethod, byteLength: number, littleEndian: boolean): number | bigint {
+    this.allocate(byteLength);
+    return this.view[`get${type}`]((this.byteOffset += byteLength) - 1, littleEndian);
+  }
+
+  private allocate(byteLength: number): void {
+    if (this.byteOffset + byteLength > this.data.byteLength){
+      throw new Error("Ran out of bytes to read, unexpectedly reached the end of the buffer");
+    }
+  }
 }
 
-class NBTReader {
-  private data!: Uint8Array;
+// write
 
-  readRoot(data: Uint8Array): RootTag {
+type WriterMethod = {
+  [K in keyof DataView]: K extends `set${infer T}` ? T : never;
+}[keyof DataView];
+
+class DataWriter {
+  byteOffset: number;
+  data: Uint8Array;
+  view: DataView;
+
+  constructor() {
+    this.byteOffset = 0;
+    this.data = new Uint8Array(1024);
+    this.view = new DataView(this.data.buffer);
+  }
+
+  writeUint8(value: number): void {
+    this.write("Uint8", 1, false, value);
+  }
+
+  writeInt8(value: number): void {
+    this.write("Int8", 1, false, value);
+  }
+
+  writeUint16(value: number, littleEndian: boolean): void {
+    this.write("Uint16", 2, littleEndian, value);
+  }
+
+  writeInt16(value: number, littleEndian: boolean): void {
+    this.write("Int16", 2, littleEndian, value);
+  }
+
+  writeUint32(value: number, littleEndian: boolean): void {
+    this.write("Uint32", 4, littleEndian, value);
+  }
+
+  writeInt32(value: number, littleEndian: boolean): void {
+    this.write("Int32", 4, littleEndian, value);
+  }
+
+  writeFloat32(value: number, littleEndian: boolean): void {
+    this.write("Float32", 4, littleEndian, value);
+  }
+
+  writeFloat64(value: number, littleEndian: boolean): void {
+    this.write("Int16", 8, littleEndian, value);
+  }
+
+  writeBigUint64(value: bigint, littleEndian: boolean): void {
+    this.write("BigUint64", 8, littleEndian, value);
+  }
+
+  writeBigInt64(value: bigint, littleEndian: boolean): void {
+    this.write("BigInt64", 8, littleEndian, value);
+  }
+
+  private write<T extends WriterMethod>(type: T, byteLength: number, littleEndian: boolean, value: ReturnType<DataView[`get${T}`]>): void;
+  private write(type: WriterMethod, byteLength: number, littleEndian: boolean, value: number | bigint): void {
+    this.allocate(byteLength);
+    this.view[`set${type}`](byteLength, value as never, littleEndian);
+  }
+
+  private allocate(byteLength: number): void {
+    const required = this.byteOffset + byteLength;
+    if (this.data.byteLength >= required) return;
+
+    let length = this.data.byteLength;
+
+    while (length < required){
+      length *= 2;
+    }
+
+    const data = new Uint8Array(length);
+    data.set(this.data, 0);
+
+    // not sure this is really needed, keeping it just in case; freezer burn
+    if (this.byteOffset > this.data.byteOffset){
+      data.fill(0, byteLength, this.byteOffset);
+    }
+
     this.data = data;
-    return this.readTag();
+    this.view = new DataView(data.buffer);
   }
-
-  readTag(): Tag {
-    const type = this.readType();
-  }
-
-  readType(): TAG {}
 }
 
 // tag
