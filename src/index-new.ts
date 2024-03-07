1ac035b3c89eb0b64eff62173e8b4c2848c4a908
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-07 10:53:47 -0800

    SNBT Stringify Migration
    Moved the stringify implementation back to functional organization! It's actually much simpler now, I like it more :)
    
    I think I'm still trying to learn to figure out where I absolutely need a class for things, versus where it might be more logical to use just functions. Having just functions for the stringify part makes more sense, because having those on their own is useful for things like UI input. I'm still not sure if the buffer-building one should be with functions, but we'll see. Should it be possible to serialize a plain tag on it's own, with individual functions? Maybe I can just have a generalized `readTag()`, `writeTag()`, `stringifyTag()`, and `parseTag()` for example. Should I also export the underlying functions on their own for individual tag types? I'm leaning towards no, for the sake of brevity things. Why import `readByte()` and `readDouble()`, when you can just use `readTag()`, which is also general? I guess for the assertion of only parsing what you intend to, could be helpful possibly. I don't want to limit the progression of NBTify's APIs by exposing too much internal business logic though, which I think is also part of my concern.

--- src/index-new.ts
+++ src/index-new.ts
@@ -18,6 +18,127 @@ await writeFile(`${process.argv[2]!}2.nbt`,writeDemo);
 
 if (process.argv.length > 2) demo();
 
+// parse SNBT
+
+// export function parse(): RootTag {}
+
+// stringify SNBT
+
+export interface StringifyOptions {
+  space?: string | number;
+}
+
+export function stringify<T extends RootTagLike = RootTag>(data: T | NBTData<T>, { space = "" }: StringifyOptions = {}): string {
+  if (data instanceof NBTData){
+    data = data.data;
+  }
+
+  space = typeof space === "number" ? " ".repeat(space) : space;
+  const level = 1;
+  return stringifyRoot(data as RootTag, space, level);
+}
+
+function stringifyRoot(value: RootTag, space: string, level: number): string {
+  const type = getTagType(value);
+  if (type !== TAG.LIST && type !== TAG.COMPOUND){
+    throw new TypeError("Encountered unexpected Root tag type, must be either a List or Compound tag");
+  }
+
+  return stringifyTag(value, space, level);
+}
+
+function stringifyTag(value: Tag, space: string, level: number): string {
+  const type = getTagType(value);
+  switch (type){
+    case TAG.BYTE: return stringifyByte(value as ByteTag | BooleanTag);
+    case TAG.SHORT: return stringifyShort(value as ShortTag);
+    case TAG.INT: return stringifyInt(value as IntTag);
+    case TAG.LONG: return stringifyLong(value as LongTag);
+    case TAG.FLOAT: return stringifyFloat(value as FloatTag);
+    case TAG.DOUBLE: return stringifyDouble(value as DoubleTag);
+    case TAG.BYTE_ARRAY: return stringifyByteArray(value as ByteArrayTag);
+    case TAG.STRING: return stringifyString(value as StringTag);
+    case TAG.LIST: return stringifyList(value as ListTag<Tag>, space, level);
+    case TAG.COMPOUND: return stringifyCompound(value as CompoundTag, space, level);
+    case TAG.INT_ARRAY: return stringifyIntArray(value as IntArrayTag);
+    case TAG.LONG_ARRAY: return stringifyLongArray(value as LongArrayTag);
+    default: throw new Error(`Encountered unsupported tag type '${type}'`);
+  }
+}
+
+function stringifyByte(value: ByteTag | BooleanTag): string {
+  return (typeof value === "boolean") ? `${value}` : `${value.valueOf()}b`;
+}
+
+function stringifyShort(value: ShortTag): string {
+  return `${value.valueOf()}s`;
+}
+
+function stringifyInt(value: IntTag): string {
+  return `${value.valueOf()}`;
+}
+
+function stringifyLong(value: LongTag): string {
+  return `${value}l`;
+}
+
+function stringifyFloat(value: FloatTag): string {
+  return `${value.valueOf()}${Number.isInteger(value.valueOf()) ? ".0" : ""}f`;
+}
+
+function stringifyDouble(value: DoubleTag): string {
+  return `${value}${!Number.isInteger(value) || value.toExponential() === value.toString() ? "" : ".0"}d`;
+}
+
+function stringifyByteArray(value: ByteArrayTag): string {
+  return `[B;${[...value].map(entry => stringifyByte(new Int8(entry))).join() satisfies string}]`;
+}
+
+function stringifyString(value: StringTag): string {
+  const singleQuoteString = escapeString(value.replace(/['\\]/g,character => `\\${character}`));
+  const doubleQuoteString = escapeString(value.replace(/["\\]/g,character => `\\${character}`));
+  return (singleQuoteString.length < doubleQuoteString.length) ? `'${singleQuoteString}'` : `"${doubleQuoteString}"`;
+}
+
+function escapeString(value: StringTag): string {
+  return value
+    .replaceAll("\b","\\b")
+    .replaceAll("\f","\\f")
+    .replaceAll("\n","\\n")
+    .replaceAll("\r","\\r")
+    .replaceAll("\t","\\t");
+}
+
+function stringifyList(value: ListTag<Tag>, space: string, level: number): string {
+  value = value.filter(isTag);
+  const fancy = (space !== "");
+  const type: TAG = (value[0] !== undefined) ? getTagType(value[0]) : TAG.END;
+  const isIndentedList = fancy && new Set<TAG>([TAG.BYTE_ARRAY,TAG.LIST,TAG.COMPOUND,TAG.INT_ARRAY,TAG.LONG_ARRAY]).has(type);
+  return `[${value.map(entry => `${isIndentedList ? `\n${space.repeat(level)}` : ""}${(() => {
+    if (getTagType(entry) !== type){
+      throw new TypeError("Encountered unexpected item type in array, all tags in a List tag must be of the same type");
+    }
+    const result = stringifyTag(entry, space, level + 1);
+    return result;
+  })() satisfies string}`).join(`,${fancy && !isIndentedList ? " " : ""}`)}${isIndentedList ? `\n${space.repeat(level - 1)}` : ""}]`;
+}
+
+function stringifyCompound(value: CompoundTag, space: string, level: number): string {
+  const fancy = (space !== "");
+  return `{${Object.entries(value).filter((entry): entry is [string,Tag] => isTag(entry[1])).map(([key,value]) => `${fancy ? `\n${(space satisfies string).repeat(level)}` : ""}${/^[0-9a-z_\-.+]+$/i.test(key) ? key : stringifyString(key)}:${fancy ? " " : ""}${(() => {
+    const result = stringifyTag(value, space, level + 1);
+    return result;
+  })() satisfies string}`).join(",")}${fancy && Object.keys(value).length !== 0 ? `\n${space.repeat(level - 1)}` : ""}}`;
+}
+
+function stringifyIntArray(value: IntArrayTag): string {
+  return `[I;${[...value].map(entry => stringifyInt(new Int32(entry))).join() satisfies string}]`;
+}
+
+function stringifyLongArray(value: LongArrayTag): string {
+  return `[L;${[...value].map(entry => stringifyLong(entry)).join() satisfies string}]`;
+}
+
 // format
 
 export type RootName = string | null;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from "./index-new.js";
 export {} from "./read.js";
 export {} from "./write.js";
 export * from "./parse.js";
-export * from "./stringify.js";
+export {} from "./stringify.js";
 export {} from "./format.js";
 export {} from "./tag.js";
 export {} from "./primitive.js";
