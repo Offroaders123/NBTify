276e0aac6113ce1d680732701e6078a874ff3515
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-06 18:49:10 -0800

    Mutable NBTData Demo!
    Glad this is a concept that works now! It's alright in this sense if things aren't completely validated, because once they get used by the API later down the line, the can be.
    
    Raining!! ☔️

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import { readFile } from "node:fs/promises";
-import { Int32, NBTData, read, write } from "../src/index.js";
+import { Int32, read, write } from "../src/index.js";
 
 const path = new URL("./nbt/hello_world.nbt",import.meta.url);
 const buffer: Buffer = await readFile(path);
@@ -8,8 +8,12 @@ const nbt = await read<any>(buffer);
 console.log(nbt);
 nbt.data.StorageVersion = new Int32(8);
 
-const newThang = new NBTData(nbt,{ endian: "little", bedrockLevel: true });
-console.log(newThang);
+const sameThang = nbt; // new NBTData(nbt,{ endian: "little", bedrockLevel: true });
+// @ts-expect-error
+sameThang.endian = "🌭";
+sameThang.endian = "little";
+sameThang.bedrockLevel = true;
+console.log(sameThang);
 
-const result = await write(newThang);
+const result = await write(sameThang);
 console.log(result);
