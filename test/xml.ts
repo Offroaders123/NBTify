85a6d5e5cfe99d7b8bd6d3db17b4ea18840e2970
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-09 11:52:09 -0700

    XML SNBT Diff
    Looks like the XML tree is properly constructed! The SNBT output diff is the same!

--- test/xml.ts
+++ test/xml.ts
@@ -1,10 +1,21 @@
 import { readFile } from "node:fs/promises";
 import { parseXML } from "../src/xml.js";
+import { parse, stringify, type RootTag } from "../src/index.js";
 
-const bigtest = new URL("./nbt/bigtest.xml", import.meta.url);
+const xml = new URL("./nbt/bigtest.xml", import.meta.url);
+const snbt = new URL("./nbt/bigtest.snbt", import.meta.url);
 
-const data: string = await readFile(bigtest, "utf-8");
-console.log(data);
+const xmlData: string = await readFile(xml, "utf-8");
+console.log(xmlData);
 
-const nodes = parseXML(data);
+const snbtData: string = stringify(parse(await readFile(snbt, "utf-8")), { space: 2 });
+
+const nodes: RootTag = parseXML(xmlData);
 console.log(nodes);
+
+const stringed: string = stringify(nodes, { space: 2 });
+console.log(snbtData);
+console.log(stringed);
+
+const diff = Buffer.compare(Buffer.from(snbtData), Buffer.from(stringed));
+console.log(diff);
