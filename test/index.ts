a0d4b179922ec40fc3e7e41e42b481674e507cfc
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-11-30 11:45:50 -0800

    Bigtest Demo for Transforms
    Yep, I don't have TypedArray values working yet.

--- test/index.ts
+++ test/index.ts
@@ -1,42 +1,16 @@
 import { readFile } from "node:fs/promises";
 import { getTagType, read } from "../src/index.js";
 
-import type { RootTag, RootTagLike, NBTData, ReadOptions } from "../src/index.js";
+import type { NBTData } from "../src/index.js";
 
-const BlockEntity = new URL("./nbt/BlockEntity.dat", import.meta.url);
-// const chunk91_ = new URL("./nbt/chunk91_.dat", import.meta.url);
+const bigtest = new URL("./nbt/bigtest.nbt", import.meta.url);
 
-const data = await readFile(BlockEntity);
+const data: Buffer = await readFile(bigtest);
 console.log(data);
 
-const options: ReadAdjacentOptions = {
-  rootName: true,
-  endian: "little",
-  compression: null,
-  bedrockLevel: false
-};
-
-// for await (const nbt of readAdjacent(data, options)) {
-//   console.log(nbt);
-// }
-
-const nbts: NBTData[] = await Array.fromAsync(readAdjacent(data, options));
-// const nbts: NBTData = (await readAdjacent(data, options).next()).value!;
-// console.log(nbts.pop());
-
-interface ReadAdjacentOptions extends Omit<ReadOptions, "strict" | "rootCheck"> {}
-
-async function* readAdjacent<T extends RootTagLike = RootTag>(data: Uint8Array, options: ReadAdjacentOptions): AsyncGenerator<NBTData<T>, void, void> {
-  let byteOffset: number = 0;
-
-  while (byteOffset < data.byteLength) {
-    // console.log(byteOffset);
-    const nbt: NBTData<T> = await read(data.subarray(byteOffset), { ...options, strict: false },
+const nbt: NBTData = await read(data, {},
     function(key, value) {
-      if (byteOffset > 850 && !("id" in this) && key !== "" && getTagType(value) === 1) console.log(this, key, value);
+      if (!("id" in this) && key !== "" && getTagType(value) === 1) console.log(this, key, value);
       return value;
     });
-    byteOffset += nbt.byteOffset!;
-    yield nbt;
-  }
-}
+// console.log(nbt);
