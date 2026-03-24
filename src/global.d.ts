380d3ede57750e13268a9f8b14c9130449bf4118
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-03-23 20:29:47 -0700

    Compression Stream Default Typings
    I think I want to come back to this project, and start from fresh. Bring the Rust concepts over to it, whether it's in file parsing/pattern matching kind of things, or merely just structuring things a bit different, now that more features are present in JS-land as a target.

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,16 +3,6 @@ declare global {
     toString(): "[object ArrayBuffer]";
   }
 
-  interface CompressionStream {
-    readonly readable: ReadableStream<Uint8Array>;
-    readonly writable: WritableStream<BufferSource>;
-  }
-
-  interface DecompressionStream {
-    readonly readable: ReadableStream<Uint8Array>;
-    readonly writable: WritableStream<BufferSource>;
-  }
-
   interface ReadableStream<R> {
     [Symbol.asyncIterator](): AsyncGenerator<R>;
   }
